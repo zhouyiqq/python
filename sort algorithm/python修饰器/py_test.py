9ae7a5bd21479a391bb430272f2c95382b11e198
zhouyiqq <2263167279@qq.com>
2021-11-07 17:25:02 +0800

    test0

--- sort algorithm/python修饰器/py_test.py
+++ sort algorithm/python修饰器/py_test.py
@@ -2,3 +2,17 @@
 # python代码仓库 was created by zy on 2021/11/7 16:39
 #这就是修饰器牛不牛比
 #客人空手来，还得请上楼，干啥都同意，有参给上楼
+def secondFloor(func):
+    def thirdFloor(info):
+        print("原函数开始执行了")
+        func(info)
+        print("原函数执行完了")
+    return thirdFloor
+@secondFloor
+def origion(info):
+    print(info)
+
+#sf = secondFloor(origion)
+#sf()
+info = "hellword"
+origion(info)
