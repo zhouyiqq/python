541c3d2af6b9e7d613a339dba81ed8234828f675
zhouyiqq <2263167279@qq.com>
2022-03-18 16:05:31 +0800

    添加监督学习

---
+++ 监督学习gymtest/test.py
@@ -0,0 +1,13 @@
+﻿# _*_coding:utf_8_*_
+# 代码仓库 was created by zy on 2022/3/18 14:29
+import gym
+env = gym.make("CartPole-v1")
+observation = env.reset()
+for _ in range(1000):
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
+
+  if done:
+    observation = env.reset()
+env.close()
